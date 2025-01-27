49657dda56c84380ff9072df50c1b0482770da48
Nitin Patel <nitpatel678@gmail.com>
2025-01-27 18:02:22 +0530

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="z-20 sticky top-3 rounded-lg ml-6 mt-2 bg-white bg-opacity-20 backdrop-blur-lg mr-6 px-8 py-4 border-2 border-violet-600 text-white shadow-md flex items-center justify-between">
+    <nav className="z-20 sticky top-1 rounded-lg ml-6 mt-1 bg-white bg-opacity-20 backdrop-blur-lg mr-6 px-8 py-4 border-2 border-violet-600 text-white shadow-md flex items-center justify-between">
       {/* Logo */}
       <div className="ml-4 text-2xl font-bold tracking-[0.25em] text-stroke text-violet-800">
         N<span className="text-violet-600 text-[1.6rem]">Patel</span>
@@ -31,7 +31,7 @@ const Navbar = () => {
       <div
         className={`${
           menuOpen ? "block" : "hidden"
-          } absolute sm:static top-[72px] left-0 w-full sm:w-auto bg-white bg-opacity-10 backdrop-blur-3xl border border-violet-600 sm:border-none sm:flex items-center flex-col sm:flex-row sm:space-x-6 mt-4 sm:mt-0 font-extrabold font-[Syne] transition-all duration-900 ease-in-out overflow-hidden`}
+          } absolute sm:static top-[72px] left-0 w-full sm:w-auto bg-violet-600 text-white bg-opacity-10 backdrop-blur-3xl border border-violet-600 sm:border-none sm:flex items-center flex-col sm:flex-row sm:space-x-6 mt-4 sm:mt-0 font-extrabold font-[Syne] transition-all duration-900 ease-in-out overflow-hidden`}
           >
         <ul
           className="flex flex-col sm:flex-row mr-3 space-y-4 sm:space-y-0 sm:space-x-6 px-8 sm:px-0"
